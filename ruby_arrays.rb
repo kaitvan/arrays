2f13e4ab9e1446651f87aa9436312ad973be4dd8
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2022-09-14 13:57:36 -0500

    Add four arrays

---
+++ ruby_arrays.rb
@@ -0,0 +1,9 @@
+students = ["Cindy", "Josiah", "Rigo"]
+
+puts students[0]
+puts students[8]
+
+items = ["milk", "bread", "eggs", "flour"]
+quantities = [1, 2, 12, 1]
+prices = [2.87, 3.99, 3.49, 0.87]
+in_cart_statuses = [true, true, false, false]
